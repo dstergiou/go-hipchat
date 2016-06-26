d5ffdd91e0ae9c6ec143649cd8a54c82666f904c
Dimitrios Stergiou <dimitrios.stergiou@netent.com>
2016-06-26 14:58:47 +0200

    Modular function

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -27,12 +26,25 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+listenPort, router))
 }
 
+// SendMessage returns the JSON notification to hipchat
+func SendMessage(w http.ResponseWriter, color string, message string) {
+	response := &Message{
+		Color:         color,
+		MessageFormat: "text",
+		Notify:        "false",
+		Message:       message,
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
 // StockPrice return the stock price for NET-B
 func StockPrice(w http.ResponseWriter, r *http.Request) {
 	var color string
 	stock, err := yquotes.NewStock("NET-B.ST", false)
 	if err != nil {
-		fmt.Println(err)
+		color = "yellow"
+		message := "Stock information cannot be retrieved"
+		SendMessage(w, color, message)
 	}
 
 	if stock.Price.Last <= stock.Price.PreviousClose {
@@ -43,12 +55,6 @@ func StockPrice(w http.ResponseWriter, r *http.Request) {
 
 	price := strconv.FormatFloat(stock.Price.Last, 'f', 2, 64)
 	oldPrice := strconv.FormatFloat(stock.Price.PreviousClose, 'f', 2, 64)
-
-	response := &Message{
-		Color:         color,
-		MessageFormat: "text",
-		Notify:        "false",
-		Message:       "Current stock price is: " + price + " SEK\nPrevious stock price was: " + oldPrice + " SEK",
-	}
-	json.NewEncoder(w).Encode(response)
+	message := "Current stock price is: " + price + " SEK\nPrevious stock price was: " + oldPrice + " SEK"
+	SendMessage(w, color, message)
 }
